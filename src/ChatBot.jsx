fcf0b0953e05b720ab08d39bd3f0d14114c2443c
souravkhilari-netizen <sourav_khilari@epam.com>
2026-04-13 16:56:11 +0530

    first commit

--- src/ChatBot.jsx
+++ src/ChatBot.jsx
@@ -1,32 +1,15 @@
-import React, { useEffect, useRef } from "react";
-import ReactWebChat from "botframework-webchat";
-
 const ChatBot = () => {
-  const webchatRef = useRef(null);
-
-  useEffect(() => {
-    const styleOptions = {
-      botAvatarInitials: "AI",
-      userAvatarInitials: "You"
-    };
-
-    window.WebChat.renderWebChat(
-      {
-        directLine: window.WebChat.createDirectLine({
-          token: "https://aa332dbd0e82ed44bfa3f251608425.5a.environment.api.powerplatform.com/copilotstudio/dataverse-backed/authenticated/bots/auto_agent_pzrmV/conversations?api-version=2022-03-01-preview"
-        }),
-        styleOptions
-      },
-      webchatRef.current
+    return (
+      <iframe
+        src="https://copilotstudio.microsoft.com/environments/aa332dbd-0e82-ed44-bfa3-f2516084255a/bots/auto_agent_pzrmV/webchat?__version__=2"
+        style={{
+          width: "100%",
+          height: "500px",
+          border: "none",
+          borderRadius: "10px"
+        }}
+      />
     );
-  }, []);
-
-  return (
-    <div
-      ref={webchatRef}
-      style={{ height: "500px", width: "100%" }}
-    />
-  );
-};
-
-export default ChatBot;
+  };
+  
+  export default ChatBot;
